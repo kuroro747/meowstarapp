905e4485c1f4b06a1b4e1cb96a54d9fe2185a2da
kuroro747 <test.zheng16@gmail.com>
2025-02-09 22:30:50 +0800

    update github action workflow for deploying to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:'/meowstarapp/', // 确保此字段正确配置为仓库名称
-  //server:{
-    //port:3003,// 设置react项目2的访问端口
-  //},
   build: {
     outDir: 'dist', // 确保输出目录为 dist
   },
+    //server:{
+    //port:3003,// 设置react项目2的访问端口
+  //},
 })
